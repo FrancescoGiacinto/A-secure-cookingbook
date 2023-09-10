3e1ec1398a1416399519b22009f5dc097654d4ac
Francesco Giacinto <francesco.giacinto@outlook.com>
2023-09-10 07:33:52 +0200

    1

--- modules/user_manager.py
+++ modules/user_manager.py
@@ -4,29 +4,13 @@ from flask import Blueprint, render_template, request, redirect, url_for
 user_manager = Blueprint('user_manager', __name__)
 
 # Sample route for user registration
-@user_manager.route('/register', methods=['GET', 'POST'])
+@user_manager.route('/singin', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # Handle user registration logic here (e.g. saving user data to database)
-        # For simplicity, this is just a placeholder.
-        username = request.form.get('username')
-        password = request.form.get('password')
-        # After registration, redirect the user to login page or dashboard.
-        # This is just a placeholder, and the actual logic will be more comprehensive.
-        return redirect(url_for('user_manager.login'))
-    return render_template('register.html')  # You would need to create this template
+    return render_template('singin.html') 
 
 # Sample route for user login
 @user_manager.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # Handle login logic here (e.g. checking credentials against the database)
-        # For simplicity, this is just a placeholder.
-        username = request.form.get('username')
-        password = request.form.get('password')
-        # If credentials are valid, log the user in and redirect them to their dashboard.
-        # This is just a placeholder, and the actual logic will be more comprehensive.
-        return redirect(url_for('views.index'))
-    return render_template('login.html')  # You would need to create this template
+    return render_template('home.html')  # You would need to create this template
 
 # Later, you can add more routes/functions related to recipe management, password reset, user profile, etc.

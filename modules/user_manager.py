d5043b10626dc7bfa260d96d549288e118e905bb
Francesco Giacinto <francesco.giacinto@outlook.com>
2023-09-10 07:34:45 +0200

    1

--- modules/user_manager.py
+++ modules/user_manager.py
@@ -11,6 +11,5 @@ def register():
 # Sample route for user login
 @user_manager.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('home.html')  # You would need to create this template
+    return render_template('home.html') 
 
-# Later, you can add more routes/functions related to recipe management, password reset, user profile, etc.

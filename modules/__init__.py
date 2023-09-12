3f2b7f7c8b428ac01b8ecf9b55a33822c718c9b2
Francesco Giacinto <francesco.giacinto@outlook.com>
2023-09-12 19:02:26 +0200

    up3

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,31 +1,55 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-from os import path
+import os 
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
-DB_PATH = os.path.join("static", "database", DB_NAME)  # Define a full path to the database file
+db = SQLAlchemy() # Initialize SQLAlchemy database object
+DB_NAME = "database.db" 
+# Create the absolute path for the database file
+DB_PATH = os.path.join(os.getcwd(), "static", "database", DB_NAME) 
 
 def create_app(config_name=None):
+    """
+    Function to create and configure the Flask app instance.
+    """
+    # Initialize the Flask app instance
     app = Flask(__name__)
     
-    # Set up the app's secret key, using an environment variable if available
+    # Configure the app's secret key using an environment variable.
+    # If the environment variable isn't set, use a fallback key.
     app.config['SECRET_KEY'] = os.environ.get('FLASK_COOKINGBOOK_SECRET_KEY', 'fallback_key_if_not_found')
     
-    # Use the full path to the database file
+    # Configure the SQLAlchemy database URI using the absolute DB_PATH
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
     
+    # Initialize the database with the app
     db.init_app(app)
 
+    # Import blueprints from other modules and register them with the app
     from .views import views
     from .user_manager import user_manager
-    from .models import User, Recipe
+    from .models import User, Recipe # Import the required models
 
-    # Register blueprints
+    # Register the blueprints with their respective url prefixes
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(user_manager, url_prefix='/user')
 
-    # Creation of the database has been moved to 'create_database' function
-    # with app.app_context():
-    #     db
+    # Return the configured app instance
+    return app
+
+def create_database(app):
+    """
+    Function to check if the database exists, 
+    and if not, create it.
+    """
+    # Create the parent directory for the database if it doesn't exist
+    parent_dir = os.path.dirname(DB_PATH)
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+        
+    # Check if the database file exists at the specified DB_PATH
+    if not os.path.exists(DB_PATH):
+        # If not, create the database tables within the app context
+        with app.app_context():
+            db.create_all()
+        print('DATABASE HAS BEEN CREATED')
+
